1c518e92a49750586db5ea31485a5e8a89b40d06
wangjinzhen <149102935+tattooidea@users.noreply.github.com>
2025-05-05 01:50:29 +0800

    add r page and add to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -9,6 +9,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 1,
+    },
+    {
+      url: `${baseUrl}/r`,
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.8,
     }
   ]
 }
